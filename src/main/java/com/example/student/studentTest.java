cd5621a7562adaf4d1d219ffe115b2638bffc713
Xiongchuanqi <www.876491913@qq.com>
2022-06-19 21:09:33 +0800

    push test

--- src/main/java/com/example/student/studentTest.java
+++ src/main/java/com/example/student/studentTest.java
@@ -10,5 +10,6 @@ public class studentTest {
         System.out.println("hello 4");
         System.out.println("master test");
         System.out.println("hot-fix test!");
+        System.out.println("push test");
     }
 }
